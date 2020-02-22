0355db8f9a96bb1278fc48ea618f08bc39d5f53e
Abbas Parpia <aparpia03@gmail.com>
2020-02-22 21:55:13 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -1,1 +1,16 @@
 require 'pry'
+class Song 
+   attr_accessor :name, :artist, :genre 
+   
+   @@count = 0 
+   
+   def initialize
+     @name
+     @artist 
+     @genre 
+     @@count = @@count + 1
+  end 
+  
+  self.count 
+  
+end 
