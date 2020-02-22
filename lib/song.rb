0af2cede268b04f82ceed30a735f398f6a645eaf
Abbas Parpia <aparpia03@gmail.com>
2020-02-22 22:16:20 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -30,13 +30,13 @@ class Song
 def self.genre_count 
     new_hash = {}
    @@genres.each do |genre|
-     binding.pry 
      if new_hash == new_hash[genre]
        new_hash[genre] +=1
      else new_hash[genre] = 1 
     end  
   end    
   new_hash 
+  binding.pry 
 end 
   
   
